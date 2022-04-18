2819ea7a4b0eff1c361478dc2a3fdedd26559c19
justin3453 <103938520+justin3453@users.noreply.github.com>
2022-04-18 12:25:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ def greeting(name):
     return f"Hello my name is {name}."
 
 
-if __name__ == "__main__":
-    name = sys.argv[1]
-    print(greeting(name))
+name = sys.argv[1]
+print(greeting(name))
 

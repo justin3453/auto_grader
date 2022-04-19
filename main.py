f7def13b7f6cc8358b275643d7acf59297f18844
justin3453 <103938520+justin3453@users.noreply.github.com>
2022-04-19 15:02:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
-import os
 import subprocess
+from students import Student
 
-file = "C:\\Users\\Justi\\PycharmProjects\\EECE2140_Final\\script.py"
-# os.system(f"python {file} Justin")
-result = subprocess.check_output(f'python {file} Justin')
-a = [elm for elm in result.decode("UTF-8")]
-result = result.decode("UTF-8").strip().strip()
-print(result)
 
-if result == "Hello my name is Justin.":
-    print("Good")
-else:
-    print("Nope")
+def get_input_output(file):
+    with open(file, "r") as f:
+        for line in f.readlines():
+            f, ip, op = line.split("|")
+            inputs.append(ip)
+            output.append(op)
 
+
+inputs = []
+output = []
+get_input_output("io.txt")
+ip1 = inputs[0]
+op1 = output[0]
+
+programs = int(input("Enter number of assignments to grade:"))
+
+s1 = Student("Mark", "Williams", 1007, "C:\\Users\\Justi\\PycharmProjects\\EECE2140_Final\\script.py")
+students = {s1.id: s1}
+
+for id, student in students.items():
+    result = subprocess.check_output(f'python {student.scripts} {ip1}')
+    result = result.decode("UTF-8").strip().strip()
+
+    print(result)
+    print(op1)
+    if result == op1:
+        student.add_points(100/programs)
+
+print(s1.grade)

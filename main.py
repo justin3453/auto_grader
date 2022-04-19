5ebceaba0a9c9459fc80de2b8ad524e0cbf1985b
justin3453 <103938520+justin3453@users.noreply.github.com>
2022-04-19 16:57:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
-import subprocess
 from students import Student
+from openpyxl import Workbook, load_workbook
+import subprocess
+import os
 
 
+# Reads txt file where pre-defined input and expected output is stored
 def get_input_output(file):
     with open(file, "r") as f:
         for line in f.readlines():
             f, ip, op = line.split("|")
             inputs.append(ip)
             output.append(op)
+
+
+# Load excel file where course roster and students' assignments are stored
+wb = load_workbook("Final_Project_Roster.xlsx")
+ws = wb.active
+
+# Student objects created will be stored in a dictionary
+students = {}
+
+# Creates Student objects using data from each row of the excel file
+for row in ws.values:
+    first, last, id, folder = row
+    student = Student(first, last, id, folder)
+    # Keeps header names out of the student dictionary
+    if type(student.id) == int:
+        students[student.id] = student
+
+
 
 
 inputs = []
@@ -18,9 +39,6 @@ op1 = output[0]
 
 programs = int(input("Enter number of assignments to grade:"))
 
-s1 = Student("Mark", "Williams", 1007, "C:\\Users\\Justi\\PycharmProjects\\EECE2140_Final\\script.py")
-students = {s1.id: s1}
-
 for id, student in students.items():
     result = subprocess.check_output(f'python {student.scripts} {ip1}')
     result = result.decode("UTF-8").strip().strip()
@@ -30,4 +48,5 @@ for id, student in students.items():
     if result == op1:
         student.add_points(100/programs)
 
-print(s1.grade)
+print(students[11007])
+

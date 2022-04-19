9fc1877f342756fe439a7306f1103e54a50fd5e5
justin3453 <103938520+justin3453@users.noreply.github.com>
2022-04-19 14:03:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 import os
+import subprocess
 
-os.system('"python C:\\Users\\Justi\\PycharmProjects\\EECE2140_Final\\script.py Justin"')
+file = "C:\\Users\\Justi\\PycharmProjects\\EECE2140_Final\\script.py"
+# os.system(f"python {file} Justin")
+result = subprocess.check_output(f'python {file} Justin')
+a = [elm for elm in result.decode("UTF-8")]
+result = result.decode("UTF-8").strip().strip()
+print(result)
+
+if result == "Hello my name is Justin.":
+    print("Good")
+else:
+    print("Nope")
 

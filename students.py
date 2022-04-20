319ddced0a3e3fffce88641b0462371834aa50c5
justin3453 <103938520+justin3453@users.noreply.github.com>
2022-04-20 16:59:34 -0400

    Update students.py

--- students.py
+++ students.py
@@ -1,10 +1,20 @@
 class Student:
-    def __init__(self, first, last):
+    def __init__(self, first, last, id, scripts=""):
+        """Initializes Student object"""
         self.first = first
         self.last = last
-        self.grade = 100
-        self.scripts = []
+        self.id = id
+        self.scripts = scripts
+        self.grade = 0
 
-    def deduct_points(self, amount):
-        self.grade -= amount
-        
+    def add_points(self, amount):
+        """Adds points to students' grade"""
+        self.grade += amount
+
+    def __str__(self):
+        """Returns students' name and grade in string format"""
+        return f"Name: {self.first} {self.last}\n" \
+               f"Student ID: {self.id}\n" \
+               f"Grade: {self.grade}\n\n"
+    
+    

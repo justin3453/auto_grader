8e42e683a7c936aa5846a5f58e814aa30e9672c4
justin3453 <103938520+justin3453@users.noreply.github.com>
2022-05-02 00:19:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,18 @@ import os
 # Creates a dictionary for the values
 def get_input_output(file):
     d = {}
-    with open(file, "r") as f:
-        lines = 0
-        for line in f.readlines():
-            script, ip, op = line.split("|")
-            d[script] = (ip, op)
-            lines += 1
-    return d, lines
+    try:
+        with open(file, "r") as f:
+            tests = 0
+            for line in f.readlines():
+                script, ip, op = line.split("|")
+                d[script] = (ip, op)
+                tests += 1
+        return d, tests
+    # Informs the user if their data file cannot be found
+    except FileNotFoundError:
+        print("I/O file does not exist.")
+        exit(1)
 
 
 # User must enter the complete file path
@@ -22,8 +27,17 @@ spreadsheet = input("Enter Excel file:")
 io_file = input("Enter text file with i/o data:")
 
 # Load Excel file where course roster and students' assignments are stored
-wb = load_workbook(spreadsheet)
-ws = wb.active
+try:
+    # Ends program is the Excel is already open
+    if os.path.isfile('~$' + spreadsheet):
+        print("You must close the excel file.")
+        exit(1)
+    wb = load_workbook(spreadsheet)
+    ws = wb.active
+except FileNotFoundError:
+    print("File does not exist.")
+    exit(1)
+
 
 # Student objects created will be stored in a dictionary
 students = {}
@@ -34,8 +48,7 @@ io, programs = get_input_output(io_file)
 
 # Creates Student objects using data from each row of the Excel file
 for row in ws.values:
-    # Unpacks each row 
-    first, last, id, folder, *data = row  
+    first, last, id, folder, *data = row
     student = Student(first.strip(), last.strip(), id, folder)
     # Keeps header names out of the student dictionary
     if type(student.id) == int:
@@ -43,29 +56,33 @@ for row in ws.values:
 
 with open("output.txt", 'w') as f_out:
     # Iterates over student dictionary
-    for s in students:
-        assignments = os.listdir(students[s].scripts)  # List of programs
-        path = os.path.abspath(students[s].scripts)  # File path for folder
-        f_out.write(f'{students[s].first} {students[s].last}:\n')
-        
+    for id, student in students.items():
+        assignments = os.listdir(student.scripts)  # List of programs
+        path = os.path.abspath(student.scripts)  # File path for folder
+        f_out.write(f'{student.first} {student.last}:\n')
+
         # Executes each one of the students' scripts
         correct = 0  # Counter for correctly made programs
         for script in assignments:
             # Name of each script must match the name written in the i/o txt file
             # Student will not receive credit if the file is named incorrectly
-            try:
-                ip, op = io[script]
-            except:
-                students[s].add_points(round(0))
+            ip, op = io[script]
 
-            result = subprocess.check_output(f"python {path}\\{script} {ip}")  # Runs command in the terminal
-            result = result.decode("UTF-8").strip()  # Formats the script's output
-            f_out.write(f'{script}: {ip} >>> {result}\n')
+            # Runs command in the terminal
+            result = subprocess.run(f"python {path}\\{script} {ip}", capture_output=True, shell=True, text=True)
+            f_out.write(f'{script}: {ip} >>> {result.stdout}')
 
             # If the output matches the desired result, points are added to grade
-            if result in op:
+            if op.strip() == result.stdout.strip():
                 correct += 1
-        students[s].add_points(round(100 * correct / programs))
+                f_out.write('Passed\n')
+            # If an error occurred in the program, it will appear in the output file
+            elif result.returncode != 0:
+                f_out.write(f'\nError: {result.stderr}\n')
+            else:
+                f_out.write('Incorrect output\n')
+
+        student.add_points(round(100 * correct / programs))
         f_out.write("\n")
 
 # Adds new column that contains all grades to spreadsheet
